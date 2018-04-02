074d103a443b56eecb97c16f9cf8e22c1b802167
prudywsh <antoineprudhomme5@gmail.com>
2018-04-02 10:54:15 +0200

    [auth] auth middleware works

--- config/routes.js
+++ config/routes.js
@@ -1,13 +1,23 @@
 const authController = require('../controllers/auth')
+const authMiddleware = require('../middlewares/auth')
 
 module.exports = (app) => {
+  // apply middlewares
+  app.use('/api', authMiddleware)
+
+  // default route
   app.get('/', (req, res) => {
     res.send('hello world !')
   })
 
+  // auth routes
   app.route('/api/auth/register')
     .post(authController.register)
-
   app.route('/api/auth/login')
     .post(authController.login)
+
+  // submissions routes
+  app.post('/api/submission', (req, res) => {
+    res.send('submission ok')
+  })
 }
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,11 @@
 const jwtHelper = require('../helpers/jwt')
 
 const authMiddleware = (req, res, next) => {
+  // exclude the following routes
+  if (req.path === '/auth/login' || req.path === '/auth/register') {
+    return next()
+  }
+
   // parse the authorisation header, to get the JWT
   const token = req.headers.authorization
     ? req.headers.authorization.split(' ')[1]
