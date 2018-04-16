ff456b8b20f6af917addc174af2f68c18fb91f74
prudywsh <antoineprudhomme5@gmail.com>
2018-04-16 14:47:13 +0200

    login/register form cleaned

--- client/components/login.js
+++ client/components/login.js
@@ -6,13 +6,16 @@ class Login extends Component {
       <form class="modal-form">
         <div class="form-group">
           <label for="email">Email address</label>
-          <input type="email" class="form-control" id="email" placeholder="Email" />
+          <input type="email" class="form-control" id="email" />
         </div>
         <div class="form-group">
           <label for="password">Password</label>
-          <input type="password" class="form-control" id="password" placeholder="Password" />
+          <input type="password" class="form-control" id="password" />
         </div>
-        <button type="button" class="btn btn-default btn-lg btn-center">Submit</button>
+        <div class="login-btn-group">
+          <button type="button" class="btn btn-default btn-lg">Login</button>
+          <button type="button" class="btn btn-default btn-lg">Register</button>
+        </div>
       </form>
     )
   }
--- client/components/loginModal.js
+++ client/components/loginModal.js
@@ -4,38 +4,21 @@ import reduce from '../reducer'
 import * as actions from '../actions'
 
 import Login from './login'
-import Register from './register'
-import LoginModalTab from './loginModalTab'
 
 import '../style/loginModal.css'
 
 @connect(reduce, actions)
 class LoginModal extends Component {
-  constructor() {
-    super()
-    this.state = {
-      tab: 'login'
-    }
-  }
-  changeTab (nextTab) {
-    this.setState({ tab: nextTab })
-  }
   render () {
     return (
       <div class="login-modal-wrapper">
         <div class="login-modal">
-          <div>
-            <button class="btn btn-default btn-lg btn-icon close-icon" onClick={this.props.toggleLoginModal}>
-              <i class="fa fa-close"></i>
-            </button>
-          </div>
+          <button class="btn btn-default btn-lg btn-icon close-icon" onClick={this.props.toggleLoginModal}>
+            <i class="fa fa-close"></i>
+          </button>
           <div class="container">
-            <div class="row login-modal-tabs">
-              <LoginModalTab text='Login' active={this.state.tab} tabName='login' onClick={this.changeTab.bind(this)} />
-              <LoginModalTab text='Register' active={this.state.tab} tabName='register' onClick={this.changeTab.bind(this)} />
-            </div>
             <div class="form-wrapper">
-              {this.state.tab === 'register' ? <Register /> : <Login />}
+              <Login />
             </div>
           </div>
         </div>
