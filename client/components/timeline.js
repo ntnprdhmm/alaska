2d7977ee7fafddf0c3cefe553d66525880661a58
prudywsh <antoineprudhomme5@gmail.com>
2018-05-02 10:46:12 +0200

    timeline with stages dates

--- client/components/timeline.js
+++ client/components/timeline.js
@@ -9,8 +9,7 @@ class Timeline extends Component {
   componentDidMount() {
     this.props.fetchStages()
   }
-  render({stages}, {}) {
-    console.log(stages)
+  render({ stages }, {}) {
     return (
       <ScrollableAnchor id="timeline">
         <section class="content-section text-center">
@@ -18,7 +17,27 @@ class Timeline extends Component {
             <div class="row">
               <div class="col-lg-10 margin-auto">
                 <h2 class="margin-auto">Timeline</h2>
-                <div>timeline</div>
+                {
+                  stages[0]
+                    ?
+                      <div class="timeline">
+                        <div class="timeline-container timeline-container--left">
+                          <div class="timeline-content">
+                            <h2>Stage 1</h2>
+                            <p><b>From</b> {new Date(parseInt(stages[0].start) * 1000).toLocaleString()}</p>
+                            <p><b>To</b> {new Date(parseInt(stages[0].end) * 1000).toLocaleString()}</p>
+                          </div>
+                        </div>
+                        <div class="timeline-container timeline-container--right">
+                          <div class="timeline-content">
+                            <h2>Stage 2</h2>
+                            <p><b>From</b> {new Date(parseInt(stages[1].start) * 1000).toLocaleString()}</p>
+                            <p><b>To</b> {new Date(parseInt(stages[1].end) * 1000).toLocaleString()}</p>
+                          </div>
+                        </div>
+                      </div>
+                    : null
+                }
               </div>
             </div>
           </div>
