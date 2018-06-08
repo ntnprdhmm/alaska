8917eaf5035cc9eeba473960ade8c29210e77321
prudywsh <antoineprudhomme5@gmail.com>
2018-06-08 09:41:29 +0200

    footer css fixed

--- client/components/footer.js
+++ client/components/footer.js
@@ -7,12 +7,14 @@ class Footer extends Component {
     return (
       <footer class="text-center">
         <div class="container">
-          <a target="_blank" href="http://www.utt.fr/en/index.html">
+          <div>
+            <a target="_blank" href="http://www.utt.fr/en/index.html">
             <img src={logo} class="img-fluid" alt="utt logo" />
           </a>
-          <span class="float-right disclaimers">
+          </div>
+          <div class="disclaimers">
             <a target="_blank" href="disclaimers.html">disclaimers</a>
-          </span>
+          </div>
         </div>
       </footer>
     )
