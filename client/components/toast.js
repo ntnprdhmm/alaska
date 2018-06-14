ec745959b531f6938bac001bcf4fd4f14cfec7bd
prudywsh <antoineprudhomme5@gmail.com>
2018-06-14 13:02:17 +0200

    hide toasts after 5 seconds

--- client/components/toast.js
+++ client/components/toast.js
@@ -1,6 +1,15 @@
 import { h, Component } from 'preact'
 
 class Toast extends Component {
+	componentDidMount() {
+		const timer = setTimeout(_ => this.props.onClick(this.props.toastKey), 5000)
+   	this.setState({ timer })
+	}
+
+	componentWillUnmount() {
+   	clearInterval(this.state.timer)
+	}
+
   render() {
     return (
       <li class={`toast toast--${this.props.type || 'success'}`}
