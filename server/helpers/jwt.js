9d44524a742e8eb01d8f1cede669bb69a98286c7
prudywsh <antoineprudhomme5@gmail.com>
2018-06-14 17:09:22 +0200

    custom message for JWT error

--- server/helpers/jwt.js
+++ server/helpers/jwt.js
@@ -3,9 +3,16 @@ const jwt = require('jsonwebtoken')
 const verify = (token) => {
   return new Promise((resolve, reject) => {
     jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
-      (err || !decodedToken)
-        ? reject(err)
-        : resolve(decodedToken)
+      // check if the token has expired
+      if (err && err.name === 'TokenExpiredError') {
+        return reject({ message: 'Your token has expired. Please logout and login back.' })
+      }
+      // check if there is another error
+      if (err || !decodedToken) {
+        return reject({ message: 'Something happened to your token. Please logout and login back.' })
+      } 
+      // all is ok, return the token
+      resolve(decodedToken)
     })
   })
 }
